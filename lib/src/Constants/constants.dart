27e7fe38022103286a631f052f175baca053fbe5
SushanShakya <sushaanshakya88@gmail.com>
2020-07-08 08:51:43 +0545

    Added text style constants

---
+++ lib/src/Constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+TextStyle kHeader = TextStyle(
+    letterSpacing: 5.0,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black);
+
+// TextStyle kTitle = TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20)
